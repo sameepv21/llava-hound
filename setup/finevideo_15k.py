2e79e470e6aa763eb9d2913dc1a342558b3ebd27
Sameep Vani <sameepvani@Sameeps-MacBook-Air.local>
2024-09-30 11:28:42 -0700

    [feat]: randomly sample parquet 15k videos

---
+++ setup/finevideo_15k.py
@@ -0,0 +1,18 @@
+import os
+import random
+import shutil
+import pandas as pd
+
+source_dir = "/scratch/svani/data/finevideo/data"
+destination_dir = "/scratch/svani/data/finevideo/15k_subset"
+os.makedirs(destination_dir, exist_ok=True)
+
+# Get list of all parquet files in the source directory
+parquet_files = [f for f in os.listdir(source_dir) if f.endswith('.parquet')]
+
+# Randomly sample 468 parquet files
+sampled_files = random.sample(parquet_files, 468)
+
+# Copy the sampled files to the destination directory
+for file in sampled_files:
+    shutil.move(os.path.join(source_dir, file), os.path.join(destination_dir, file))
