2c6c430636824858b86cd0d860fb087be592d033
Sameep Vani <sameepvani@Sameeps-MacBook-Air.local>
2024-10-01 00:20:11 -0700

    [feat]: updated mixtral script

--- dpo_scripts/generate_pref.py
+++ dpo_scripts/generate_pref.py
@@ -1,5 +1,14 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
+import numpy as np
+import pandas as pd
+from pathlib import Path 
+import torch
+import time
+import os
+from tqdm import tqdm
+import json
+import argparse
 
 device = "cuda" # the device to load the model onto
 
@@ -21,24 +30,11 @@ template_ = """
                 \n\nInput: {}\nOutput: 
             """
 
-
-import numpy as np
-import pandas as pd
-from pathlib import Path 
-import torch
-import time
-import os
-from tqdm import tqdm
-
-import argparse
-
 parser = argparse.ArgumentParser()
-# parser.add_argument('--start_idx', required=True, type=int)
-# parser.add_argument('--end_idx', required=True, type=int)
-# parser.add_argument('--batch_idx', required=True, type=int)
-# parser.add_argument('--total_batch', default=10, type=int)
-# parser.add_argument('--save_interval', default=100, type=int, help="Interval of iterations to save the output JSON")
-parser.add_argument('--batch_size', default=33, type=int)
+parser.add_argument('--start_idx', type=int, default=0)
+parser.add_argument('--end_idx', type=int, default=15101)
+parser.add_argument('--save_interval', default=100, type=int, help="Interval of iterations to save the output JSON")
+parser.add_argument('--batch_size', default=32, type=int)
 parser.add_argument('--data_path', type=str, default="/scratch/svani/data/finevideo", help='path to the save directory')
 parser.add_argument('--save_path', type=str, default="/scratch/svani/data/finevideo", help='path to the save directory')
 args = parser.parse_args()
@@ -46,21 +42,12 @@ args = parser.parse_args()
 save_path = args.save_path
 os.makedirs(save_path, exist_ok=True)
 
-org_path = os.path.join(args.data_path, "finevideo-15k-descriptions.json")
-gpt_path = os.path.join(args.data_path, "finevideo-15k-dpo-descriptions.json")
+org_path = os.path.join(args.data_path, "finevideo-15k-description.json")
+gpt_path = os.path.join(args.data_path, "finevideo-15k-gpt.txt")
 
-org_df = pd.read_csv(org_path, header=None, names=["image_id", "caption", "url"])
-data = []
-with open(gpt_path) as h:
-    tmp_ = h.readlines()
-    for t in tmp_:
-        data.append(t.strip())
+org_df = pd.read_json(org_path, header=None, names=["name", "caption"])
+data = org_df['caption'].tolist()
 
-assert len(org_df["caption"].tolist()) == len(data)
-
-# each_batch_length = len(data) // args.total_batch
-# start_idx = each_batch_length * args.batch_idx
-# end_idx = start_idx + each_batch_length
 start_idx = args.start_idx 
 end_idx = args.end_idx
 
@@ -69,7 +56,6 @@ data = data[start_idx:end_idx]
 
 print(len(org_df), len(data))
 
-import json
 def update_json_file(filepath, new_data):
     try:
         with open(filepath, 'r+') as file:
@@ -102,16 +88,13 @@ while ci <= len(data):
 
     for i in range(len(batch)):
         new_captions.append({
-            "image_id": org_df.loc[ci+i, "image_id"], 
-            "url": org_df.loc[ci+i, "url"], 
+            "name": org_df.loc[ci+i, "name"], 
             "caption": org_df.loc[ci+i, "caption"], 
-            "language-rewrite": batch[i], 
-            "Mixtral_instruct_v02": decoded[i][len(prompts[i]):].strip(), 
             "negative_caption": decoded[i][len(prompts[i]):].strip().split("Input")[0].split("Output")[0].split(".")[0].strip() + "."
         })
 
     if len(new_captions) // args.save_interval != 0:
-        update_json_file(os.path.join(save_path, f"cc12m_batch-start_idx-{args.start_idx}-end_idx-{args.end_idx}.json"), new_captions)
+        update_json_file(os.path.join(save_path, f"finevideo-dpo-15k-descriptions.json"), new_captions)
         new_captions = []
         save_ck += 1
 
@@ -121,4 +104,4 @@ while ci <= len(data):
 pbar.close()
 
 if len(new_captions)>0:
-    update_json_file(os.path.join(save_path, f"cc12m_batch-start_idx-{args.start_idx}-end_idx-{args.end_idx}.json"), new_captions)
+    update_json_file(os.path.join(save_path, f"finevideo-dpo-15k-descriptions.json"), new_captions)
