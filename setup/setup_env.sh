b452bda0c93b11a5fb5c222603b3e6075cbfd823
sameepv21 <sameep0112vani@gmail.com>
2024-10-22 16:44:03 +0000

    [chore]: added required libraries

--- setup/setup_env.sh
+++ setup/setup_env.sh
@@ -2,10 +2,14 @@ cd setup
 echo huggingface for downloading large file
 source install_hf.sh
 echo install requirements
-pip install -r requirements.txt # removed sudo for sol
+pip install -r requirements.txt
 pip install --upgrade numpy==1.26.4
 pip install --upgrade wandb==0.17.0
 pip install --upgrade torch==2.1.2
 pip install --upgrade transformers==4.37.0
 pip install --upgrade torchvision==0.16.2
 pip install --upgrade datasets==3.0.0
+pip install gdown
+pip install wheel
+pip install flash-attn --no-build-isolation
+pip install nvitop
