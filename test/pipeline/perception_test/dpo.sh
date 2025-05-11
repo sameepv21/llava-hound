b660d9f84a2e321ad01a18bb2d01c26444a32e89
sameepv21 <sameep0112vani@gmail.com>
2025-05-10 18:46:22 -0700

    [feat]: sync

---
+++ test/pipeline/perception_test/dpo.sh
@@ -0,0 +1,25 @@
+output_model_name=internvideo_dpo
+model_path=OpenGVLab/InternVideo2_5_Chat_8B
+model_base="None"
+load_peft=/scratch/svani/experiments/internvideo_lhdpo
+
+TEST_DATA_DIR=/scratch/svani/benchmarks/ptest
+TEST_RESULT_DIR=/scratch/svani/evaluation
+
+data_name=perceptiontest
+data_path=$TEST_DATA_DIR/annotation.json
+output_path=$TEST_RESULT_DIR/perceptiontest/inference_test_official
+
+cache_dir=/scratch/svani/.cache
+VIDEO_DATA_DIR=/scratch/svani/benchmarks/ptest
+
+bash test/inference/inference_test_qa.sh \
+$data_path \
+$output_path/${output_model_name} \
+$model_path \
+$model_base \
+$load_peft \
+$cache_dir \
+$VIDEO_DATA_DIR
+
+bash test/eval/eval_official_zeroshot_qa.sh $output_path/${output_model_name}.jsonl ${TEST_RESULT_DIR}/${data_name}/eval_test_official
